cdba3ba87cad532b07fc0bbcd14748480a9896a8
fe-sagae <134615728+fe-sagae@users.noreply.github.com>
2023-05-25 09:57:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥣 Omega 3 and Blueblerry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
