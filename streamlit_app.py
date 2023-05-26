1eb006492d9c2f52774cc96110f69fd85dec95c2
fe-sagae <134615728+fe-sagae@users.noreply.github.com>
2023-05-25 22:31:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from Strealit')")
+
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
@@ -27,7 +27,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from Strealit')")
+
 
 # take the json and normalize it  
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
